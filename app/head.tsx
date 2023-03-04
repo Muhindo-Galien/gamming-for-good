49258b9574b01da1ff0a88cc7279e1b4d17eb190
Muhindo-Galien <muhindogalien512@gmail.com>
2023-03-04 14:47:19 +0300

    base setup

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>G4G</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Gamming for good is an gamming store" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type pageProps = {
+  
+};
+
+const Homepage:React.FC<pageProps> = () => {
+  
+  return <div>Have a good coding</div>
+}
+export default Homepage;
