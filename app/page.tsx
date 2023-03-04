4baf1c32b92a9f3d7117446f83967a7700ada947
Muhindo-Galien <muhindogalien512@gmail.com>
2023-03-04 17:50:58 +0300

    header added

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,6 @@ type pageProps = {
 
 const Homepage:React.FC<pageProps> = () => {
   
-  return <div>Have a good coding</div>
+  return <div className='pt-28'>Have a good coding</div>
 }
 export default Homepage;
